f05ecdaea816352414172ee7dd6f24b30a007fd1
Mario Germoliato <mgermoliato@yahoo.com.br>
2019-11-08 16:06:55 -0300

    Implementação do sistema de câmera

---
+++ CruzVermelha/Assets/Scripts/ControladorCamera.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorCamera : MonoBehaviour
+{
+    public Transform posCamera;
+    public Transform[] localizacoes;
+    public int indiceLocal;
+    
+    public float alturaCamera;
+    public float eixoX;
+    public float velocidadeCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        alturaCamera = posCamera.position.y;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        eixoX = localizacoes[indiceLocal].transform.position.x;
+        moverCamera();
+    }
+
+    public void moverCamera()
+    {
+        Vector3 posicaoDestinoCamera = new Vector3(eixoX, alturaCamera, -10);
+        posCamera.transform.position = Vector3.Lerp(posCamera.transform.position, posicaoDestinoCamera, velocidadeCamera * Time.deltaTime);
+    }
+
+    public void proximoCaso()
+    {
+        if (indiceLocal < 4)
+        {
+            indiceLocal += 1;
+        }
+
+    }
+
+    public void voltaCaso()
+    {
+        if (indiceLocal > 0)
+        {
+            indiceLocal -= 1;
+        }
+    }
+
+
+}
