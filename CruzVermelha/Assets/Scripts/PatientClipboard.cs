f1b43a4b12067943aa30f71beb26e484d6e0a246
Vitor <vtm997@gmail.com>
2019-11-12 19:45:46 -0300

    Menu de seleção de paciente, prancheta

---
+++ CruzVermelha/Assets/Scripts/PatientClipboard.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class PatientClipboard : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI clipboardText;
+
+    public void ActivateAndSetToPatientCase(Patient patient)
+    {
+        Case patientCase = patient.PatientCase;
+        clipboardText.text = "";
+        if(patientCase.burnInLeftHand)
+        {
+            clipboardText.text += "Queimadura na mão esquerda\n";
+        }
+        if(patientCase.heartAttack)
+        {
+            clipboardText.text += "Tendo um ataque cardiaco\n";
+        }
+        if(patientCase.choking)
+        {
+            clipboardText.text += "Engasgando\n";
+        }
+        gameObject.SetActive(true);
+    }
+}
