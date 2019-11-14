8b09500dc8dea1b5c8f8b79af7f17d2fff59d4a9
Vitor <vtm997@gmail.com>
2019-11-14 10:19:54 -0300

    Teste pro celular

--- CruzVermelha/Assets/Scripts/UIManager.cs
+++ CruzVermelha/Assets/Scripts/UIManager.cs
@@ -6,15 +6,45 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     public GameObject painelDica;
+    public GameObject painelVida;
+    public GameObject paineis;
+    private AdMobManager _AdMobManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        painelDica.SetActive(false);
+        painelVida.SetActive(false);
+        _AdMobManager = FindObjectOfType(typeof(AdMobManager)) as AdMobManager;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    public void DicaRecebida()
+    {
+        painelDica.SetActive(false);
+    }
+
+    public void ReceberReward()
+    {
+        painelVida.SetActive(false);
+    }
+
+    public void MostrarDica()
+    {
+        paineis = painelDica;
+        _AdMobManager.ShowRewardedAd();
+    }
+
+    public void PropagandaVida()
+    {
+        paineis = painelVida;
+        _AdMobManager.ShowRewardedAd();
+    }
+
 }
