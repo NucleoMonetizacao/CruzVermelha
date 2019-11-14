7846c5dee5f6d4f592db30661385de9ab1e4fc7b
Vitor <vtm997@gmail.com>
2019-11-14 07:36:58 -0300

    Auto stash before merge of "master" and "origin/master"

---
+++ CruzVermelha/Assets/Scripts/PhoneScreenDirector.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoneScreenDirector : MonoBehaviour
+{
+    PhoneStates currentPhoneState;
+
+    [SerializeField]
+    GameObject MainPhoneScreen;
+
+    [SerializeField]
+    GameObject ClipboardPhoneScreen;
+
+    [SerializeField]
+    GameObject SOSPhoneScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetPhoneStateToMain();
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    /*
+    void SetPhoneStateTo(PhoneStates newState)
+    {
+
+
+    }
+    */
+
+    private void SetPhoneStateToMain()
+    {
+        
+    }
+
+    public enum PhoneStates
+    {
+        Main, Clipboard, SOS
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
