2414c7929b58a4d86231d0d5ac84c0449e0ecc77
Mario Germoliato <mgermoliato@yahoo.com.br>
2019-11-14 01:51:25 -0300

    Implementação do sistema de propagandas
    Sistema de propaganda implementado, possibilitando que o jogador receba dicas ou energia extra.

--- CruzVermelha/Assets/Scripts/UIManager.cs
+++ CruzVermelha/Assets/Scripts/UIManager.cs
@@ -6,9 +6,16 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     public GameObject painelDica;
+    public GameObject painelVida;
+    public GameObject paineis;
+    private AdMobManager _AdMobManager;
+    
     // Start is called before the first frame update
     void Start()
     {
+        painelDica.SetActive(false);
+        painelVida.SetActive(false);
+        _AdMobManager = FindObjectOfType(typeof(AdMobManager)) as AdMobManager;
         
     }
 
@@ -17,4 +24,27 @@ public class UIManager : MonoBehaviour
     {
         
     }
+
+    public void DicaRecebida()
+    {
+        painelDica.SetActive(false);
+    }
+
+    public void ReceberReward()
+    {
+        painelVida.SetActive(false);
+    }
+
+    public void MostrarDica()
+    {
+        paineis = painelDica;
+        _AdMobManager.ShowRewardedAd();
+    }
+
+    public void PropagandaVida()
+    {
+        paineis = painelVida;
+        _AdMobManager.ShowRewardedAd();
+    }
+  
 }
