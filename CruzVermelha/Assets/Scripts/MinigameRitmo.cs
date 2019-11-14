43644bffef190e9840d7a75ee0a52dcb38feef3e
Khalil Yamada Gangeme <khalil.yamada@hotmail.com>
2019-11-14 09:53:47 -0300

    Adicionando texto no minigame

--- CruzVermelha/Assets/Scripts/MinigameRitmo.cs
+++ CruzVermelha/Assets/Scripts/MinigameRitmo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MinigameRitmo : MonoBehaviour
 {
@@ -8,6 +9,13 @@ public class MinigameRitmo : MonoBehaviour
     [Header("Bool")]
 
     private bool check;
+
+
+
+    [Header("Texto")]
+
+    [SerializeField]
+    private Text textoFeedback;
 
 
     
@@ -22,42 +30,42 @@ public class MinigameRitmo : MonoBehaviour
     {
         if(seta.position.y >= 2.33f)
         {
-            Debug.Log("Acertou Vermelho");
+            textoFeedback.text = "Acertou Vermelho";
             return;
         }
         if (seta.position.y <= 2.32f && seta.position.y >= 1.57f)
         {
-            Debug.Log("Acertou Laranja");
+            textoFeedback.text = "Acertou Laranja";
             return;
         }
         if (seta.position.y <= 1.56f && seta.position.y >= 0.8f)
         {
-            Debug.Log("Acertou Amarelo");
+            textoFeedback.text = "Acertou Amarelo";
             return;
         }
         if (seta.position.y <= 0.7f && seta.position.y >= 0.06f)
         {
-            Debug.Log("Acertou Verde Claro");
+            textoFeedback.text = "Acertou Verde Claro";
             return;
         }
         if (seta.position.y <= 0.05f && seta.position.y >= -0.722f)
         {
-            Debug.Log("Acertou Verde Escuro");
+            textoFeedback.text = "Acertou Verde Escuro";
             return;
         }
         if (seta.position.y <= -0.721f && seta.position.y >= -1.48f)
         {
-            Debug.Log("Acertou Azul Claro");
+            textoFeedback.text = "Acertou Azul Claro";
             return;
         }
         if (seta.position.y <= -1.49f && seta.position.y >= -2.24f)
         {
-            Debug.Log("Acertou Azul Escuro");
+            textoFeedback.text = "Acertou Azul Escuro";
             return;
         }
         if (seta.position.y <= -2.25f && seta.position.y >= -3f)
         {
-            Debug.Log("Acertou Roxo");
+            textoFeedback.text = "Acertou Roxo";
             return;
         }
     }
