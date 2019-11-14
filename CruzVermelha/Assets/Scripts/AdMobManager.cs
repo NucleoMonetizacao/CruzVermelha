a67b9be0e73f6d22d55893fac50cccbced759459
Mario Germoliato <mgermoliato@yahoo.com.br>
2019-11-13 22:12:09 -0300

    Scene level
    Deletado a scene level, implementação parcial do código de Ad

--- CruzVermelha/Assets/Scripts/AdMobManager.cs
+++ CruzVermelha/Assets/Scripts/AdMobManager.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using GoogleMobileAds.Api;
 public class AdMobManager : MonoBehaviour
 {
+    private RewardBasedVideoAd rewardedAd;
+    private string rewardedAdID = "ca-app-pub-3940256099942544~3347511713";
     // Start is called before the first frame update
     void Start()
     {
-        
+        rewardedAd = RewardBasedVideoAd.Instance;
+        RequestRewardedAd();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RequestRewardedAd()
     {
-        
+        AdRequest request = new AdRequest.Builder().Build();
+
+        rewardedAd.LoadAd(request, rewardedAdID);
+    }
+
+    public void ShowRewardedAd()
+    {
+        if (rewardedAd.IsLoaded())
+        {
+            rewardedAd.Show();
+        }
+        else
+        {
+            Debug.Log("Rewarded ad not loaded");
+        }
     }
 }
---
+++ CruzVermelha/Assets/Scripts/UIManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject painelDica;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
