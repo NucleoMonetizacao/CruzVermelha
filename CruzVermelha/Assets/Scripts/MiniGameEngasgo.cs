908e71e9ef51237f3c2164afebfae068566a791d
Mario Germoliato <mgermoliato@yahoo.com.br>
2019-11-20 16:15:24 -0300

    MiniGames de Queimadura e engasgo
    As mecânicas de ambos Minigames foram feitas, ambos se encontram como prefabs.

---
+++ CruzVermelha/Assets/Scripts/MiniGameEngasgo.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MiniGameEngasgo : MonoBehaviour
+{
+    [Header("Variaveis da barra e cursor")]
+    public GameObject cursor;
+    public float valorBarra;
+    public float subtracaoBarra;
+    public float adicaoBarra;
+
+    [Header("Variaveis resultado")]
+    public Text txtResultado;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        ControleCursor();
+    }
+
+    public void AcaoEngasgo()
+    {
+        valorBarra += adicaoBarra;
+    }
+
+    public void ControleCursor()
+    {
+        cursor.transform.position = new Vector3(cursor.transform.position.x, valorBarra, cursor.transform.position.z);
+
+        valorBarra -= subtracaoBarra;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("FimEngasgo"))
+        {
+            print("papagaio");
+            txtResultado.text = "Você conseguiu!";
+            subtracaoBarra = 0;
+            adicaoBarra = 0;
+        }
+        else if (collision.CompareTag("PerdeuEngasgo"))
+        {
+            txtResultado.text = "O paciente morreu";
+            subtracaoBarra = 0;
+            adicaoBarra = 0;
+        }
+    }
+}
---
+++ CruzVermelha/Assets/Scripts/MiniGameQueimadura.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MiniGameQueimadura : MonoBehaviour
+{
+    public GameObject player, forno, geladeira, pia, microondas;
+
+   public  Vector2 playerPosicaoInicial;
+
+    public Text txtResultadoMiniGame; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPosicaoInicial = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DragPlayer()
+    {
+        player.transform.position = Input.mousePosition;
+    }
+
+    public void DropPlayer()
+    {
+        float Distance = Vector3.Distance(player.transform.position, pia.transform.position);
+        if(Distance<50)
+        {
+            player.transform.position = pia.transform.position;
+            txtResultadoMiniGame.text = "Você tratou corretamente a queimadura";
+
+        }
+        else
+        {
+            player.transform.position = playerPosicaoInicial;
+            txtResultadoMiniGame.text = "Este não é o local correto para tratar uma queimadura";
+            StartCoroutine("LimpaResposta");
+        }
+    }
+
+    IEnumerator LimpaResposta()
+    {
+        yield return new WaitForSeconds(2);
+        txtResultadoMiniGame.text = " ";
+    }
+
+
+}
